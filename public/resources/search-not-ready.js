43c373a14f20d3fbdac05d952d6de8090e1f0004
David Corne <davidcorne@gmail.com>
2017-01-27 16:55:33 +0000

    Move javascript out of template.

---
+++ public/resources/search-not-ready.js
@@ -0,0 +1,4 @@
+//=============================================================================
+// The search index hasn't been built yet, refresh every second till it has.
+
+setTimeout(function() {window.location.reload(true);}, 1000);
