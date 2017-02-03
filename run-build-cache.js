b7bde17ea1170b2b963ee24b5314fc45e69e04f0
David Corne <davidcorne@gmail.com>
2017-02-03 16:44:22 +0000

    Remove commented out code in run-build-cache.

--- run-build-cache.js
+++ run-build-cache.js
@@ -1,7 +1,4 @@
 'use strict'
 
 const buildCache = require('./build-cache');
-// <nnn> 
-// <nnn> NO
-// <nnn> setTimeout(function() {buildCache.buildCache('public/recipes');}, 30000);
 buildCache.buildCache('public/recipes');
