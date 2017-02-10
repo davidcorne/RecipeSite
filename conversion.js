ebe8e07047efcfa0287827b13dc133ec270c0017
David Corne <david.corne@autodesk.com>
2017-02-10 15:04:11 +0000

    Add some ingredients to conversion.

--- conversion.js
+++ conversion.js
@@ -15,5 +15,65 @@ module.exports.conversions = [
         type:       'volume',
         cup:        240
     },
+    {
+        ingredient: 'Sugar',
+        type:       'weight',
+        cup:        0
+    },
+    {
+        ingredient: 'Double Cream',
+        type:       'volume',
+        cup:        0
+    },
+    {
+        ingredient: 'Dulce de Leche',
+        type:       'weight',
+        cup:        0
+    },
+    {
+        ingredient: 'Grand Marnier',
+        type:       'volume',
+        cup:        0
+    },
+    {
+        ingredient: 'White Chocolate',
+        type:       'weight',
+        cup:        0
+    },
+    {
+        ingredient: 'Breadcrumbs',
+        type:       'weight',
+        cup:        0
+    },
+    {
+        ingredient: 'Parmesan',
+        type:       'weight',
+        cup:        0
+    },
+    {
+        ingredient: 'Chives',
+        type:       'weight',
+        cup:        0
+    },
+    {
+        ingredient: 'Butter',
+        type:       'weight',
+        cup:        0
+    },
+    {
+        ingredient: 'Maple Syrup',
+        type:       'volume',
+        cup:        0
+    },
+    {
+        ingredient: 'Vinegar',
+        type:       'volume',
+        cup:        0
+    },
+    {
+        ingredient: 'Peanut Butter',
+        type:       'weight',
+        cup:        0
+    },
 ].sort(sorter);
 
