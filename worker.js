3fb206f3e7f61cb0df8b6675c3506128216e10f1
David Corne <david.corne@autodesk.com>
2017-02-13 17:03:54 +0000

    Refactor templates, to move to mobile friendly routing.

--- worker.js
+++ worker.js
@@ -12,11 +12,13 @@ const fileList = require('./file-list');
 let index = {};
 
 // Compile a function
-const indexTemplate = pug.compileFile('template/index.pug');
-const searchTemplate = pug.compileFile('template/search.pug');
-const searchNotReadyPage = pug.compileFile('template/search-not-ready.pug');
-const partialLoadTemplate = pug.compileFile('template/partial-search.pug');
-const conversionTemplate = pug.compileFile('template/conversion.pug');
+const templates = {
+    'index': pug.compileFile('template/index.pug'),
+    'search': pug.compileFile('template/search.pug'),
+    'search-not-ready': pug.compileFile('template/search-not-ready.pug'),
+    'partial-load': pug.compileFile('template/partial-search.pug'),
+    'conversion': pug.compileFile('template/conversion.pug'),
+};
 
 const app = express();
 const http = require('http').Server(app);
@@ -61,19 +63,30 @@ const logRequest = function(request) {
 }
 
 //=============================================================================
+const sendTemplate = function(request, response, key, data) {
+    // don't do anything fancy yet
+    response.send(templates[key](data));
+}
+
+//=============================================================================
 app.get('/', function(request, response) {
     logRequest(request);
     const locals = {
         recipes: fileList.generateFileList()
     };
-    response.send(indexTemplate(locals));
+    sendTemplate(request, response, 'index', locals);
 });
 
 //=============================================================================
 app.get('/conversion', function(request, response) {
     logRequest(request);
     // A list of the conversions that we cover.
-    response.send(conversionTemplate({conversions: conversion.conversions}));
+    sendTemplate(
+        request, 
+        response, 
+        'conversion', 
+        {conversions: conversion.conversions}
+    );
 });
 
 //=============================================================================
@@ -87,18 +100,18 @@ app.get('/search', function(request, response) {
     logRequest(request);
     if (Object.keys(index).length === 0) {
         // Send a search results not ready signal.
-        response.send(searchNotReadyPage());
+        sendTemplate(request, response, 'search-not-ready', {});
     } else {
         // We've got a search index, actually search it.
         const timer = utils.timer().start();
         const results = search.search(request.query.query, index);
         timer.stop();
-        const template = partialLoad ? partialLoadTemplate : searchTemplate;
-        response.send(template({
+        const key = partialLoad ? 'partial-load' : 'search';
+        sendTemplate(request, response, key, {
             results: results,
             query: request.query.query,
             time: timer.milliseconds
-        }));
+        });
     }
 });
 
