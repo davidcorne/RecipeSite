8118c365eea895d4c090cb91685bb7fd3cf6f347
David Corne <david.corne@autodesk.com>
2018-07-12 16:20:49 +0100

    Add an occurrences function to utils.

--- utils.js
+++ utils.js
@@ -42,6 +42,39 @@ const timer = function () {
   return this
 }
 
+/** Function that count occurrences of a substring in a string;
+ * @param {String} string               The string
+ * @param {String} subString            The sub string to search for
+ * @param {Boolean} [allowOverlapping]  Optional. (Default:false)
+ *
+ * @author Vitim.us https://gist.github.com/victornpb/7736865
+ * @see Unit Test https://jsfiddle.net/Victornpb/5axuh96u/
+ * @see http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string/7924240#7924240
+ */
+function occurrences (string, subString, allowOverlapping) {
+  string += ''
+  subString += ''
+  if (subString.length <= 0) {
+    return (string.length + 1)
+  }
+
+  let n = 0
+  let pos = 0
+  let step = allowOverlapping ? 1 : subString.length
+
+  while (true) {
+    pos = string.indexOf(subString, pos)
+    if (pos >= 0) {
+      ++n
+      pos += step
+    } else {
+      break
+    }
+  }
+  return n
+}
+
+module.exports.occurrences = occurrences
 module.exports.pathToLabel = pathToLabel
 module.exports.cachePath = cachePath
 module.exports.walk = walk
