fd65506e8636c6ba134fda872ba4e03ed22de56e
David Corne <david.corne@autodesk.com>
2018-10-25 11:24:21 +0100

    Write tags from run-build-extra

--- edit-tags.js
+++ edit-tags.js
@@ -2,16 +2,20 @@
 const utils = require('./utils')
 const tags = require('./tags')
 
-utils.foreachRecipe('public/recipes', function (recipePath) {
-  if (!tags.tagsExists(recipePath)) {
-    tags.initialiseTags(recipePath)
-  }
-  const t = tags.readTagsSync(recipePath)
-  if (recipePath.search('Vegan') !== -1) {
-    const recipeTags = t['tags']
-    if (!recipeTags.includes('vegan')) {
-      recipeTags.push('vegan')
+const initialiseAllTags = function () {
+  utils.foreachRecipe('public/recipes', function (recipePath) {
+    if (!tags.tagsExists(recipePath)) {
+      tags.initialiseTags(recipePath)
     }
-  }
-  tags.writeTagsSync(recipePath, t)
-})
+    const t = tags.readTagsSync(recipePath)
+    if (recipePath.search('Vegan') !== -1) {
+      const recipeTags = t['tags']
+      if (!recipeTags.includes('vegan')) {
+        recipeTags.push('vegan')
+      }
+    }
+    tags.writeTagsSync(recipePath, t)
+  })
+}
+
+module.exports.initialiseAllTags = initialiseAllTags
--- run-build-extra.js
+++ run-build-extra.js
@@ -2,6 +2,8 @@
 
 const log = require('./log')
 const buildCache = require('./build-cache')
+const editTags = require('./edit-tags')
 
 log.info('Logging level: ' + log.level())
 buildCache.buildCache('public/recipes')
+editTags.initialiseAllTags()
