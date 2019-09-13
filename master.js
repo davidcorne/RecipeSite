6dfaf7f9d723a1988dc8b2a772b181e45603c2d1
davidcorne <davidcorne@gmail.com>
2019-09-13 13:02:18 +0100

    Read the git commit

--- master.js
+++ master.js
@@ -1,9 +1,11 @@
 'use strict'
 const cluster = require('cluster')
+const fs = require('fs')
 const log = require('./log')
 const os = require('os')
 
 const numWorkers = (process.env.WORKERS || os.cpus().length)
+let gitCommitSha
 
 const startWorkers = function () {
   let currentWorkers = Object.keys(cluster.workers).length
@@ -35,12 +37,23 @@ const setupCallbacks = function () {
   // to read the search index.
   cluster.on('online', function (worker) {
     worker.process.send('load-search-index')
-    worker.send({git_commit_sha: 'abc-123'})
+    worker.send({git_commit_sha: gitCommitSha})
   })
+}
+
+const getGitCommitShaSync = function () {
+  const rev = fs.readFileSync('.git/HEAD').toString()
+  if (rev.indexOf(':') === -1) {
+    return rev
+  } else {
+    const branch = rev.substring(5).trim()
+    return fs.readFileSync('.git/' + branch).toString().trim()
+  }
 }
 
 const start = function () {
   log.info('Logging level: ' + log.level())
+  gitCommitSha = getGitCommitShaSync()
   setupCallbacks()
   startWorkers()
 }
