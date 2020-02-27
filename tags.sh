f8df9b9dca754a35fd918e18694056c8b1a9558a
David Corne <david.corne@autodesk.com>
2020-02-27 16:21:05 +0000

    Use short date format

--- tags.sh
+++ tags.sh
@@ -1,4 +1,4 @@
 find public/recipes/ \( -name "*.pdf" -o -name "*.html" -o -name "*.jpg" \) -exec bash -c "
-echo \"{} \`git log -1 --pretty=\"format:%ci\" \"{}\"\`\"
+echo \"\\\"{}\\\" \`git log -1 --pretty=\"format:%cd\" --date=short \"{}\"\`\"
 " \;
 
