52fa7b6988f318f8fabc2b674b91a2e807f6a298
David Corne <david.corne@autodesk.com>
2020-07-21 17:16:56 +0100

    Add a user config file

--- configuration.js
+++ configuration.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const fs = require('fs')
 const os = require('os')
 
 const configuration = {
@@ -8,5 +9,15 @@ const configuration = {
   herokuSlugCommit: process.env.HEROKU_SLUG_COMMIT,
   port: (process.env.PORT || 3000)
 }
+
+// Override those properties with the user config
+fs.stat('./user_config.json', function (error) {
+  if (!error) {
+    const userConfig = require('./user_config')
+    for (const property in userConfig) {
+      configuration[property] = userConfig[property]
+    }
+  }
+})
 
 module.exports = configuration
