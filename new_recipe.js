9a688f500583a80adc5ee49616da09f969a15efa
davidcorne <davidcorne@gmail.com>
2021-02-17 10:49:52 +0000

    Start new_recipe restructure

--- new_recipe.js
+++ new_recipe.js
@@ -25,11 +25,25 @@ const newRecipe = function (name, directory, callback) {
   fs.writeFile(fileName, md, callback)
 }
 
+const newRecipeFromUrl = function (url, directory, callback) {
+  callback()
+}
+
 const main = function () {
   const program = require('commander')
   program
     .version('1.0.0')
+    .option('-u, --url <url>', 'Create a recipe from that URL')
     .parse(process.argv)
+  const url = program.opts().url
+  if (url) {
+    newRecipeFromUrl(url, '.', function (error) {
+      if (error) {
+        throw error
+      }
+      console.log('Wrote recipe from', url)
+    })
+  }
   program.args.forEach(recipe => {
     newRecipe(recipe, '.', function (error) {
       if (error) {
