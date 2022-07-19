90dd08af30f18693ed2dcbde2eec0305823c684b
davidcorne <davidcorne@gmail.com>
2022-07-19 23:37:22 +0100

    Show the dropdown in other themes

--- public/resources/themes.js
+++ public/resources/themes.js
@@ -2,14 +2,23 @@ themes = {}
 
 themes.setTheme = function (theme) {
     // Create new link Element
-    var link = document.createElement('link');
+    var themeCSS = document.createElement('link');
 
     // set the attributes for link element
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = '/public/resources/themes/' + theme + '.min.css';
+    themeCSS.rel = 'stylesheet';
+    themeCSS.type = 'text/css';
+    themeCSS.href = '/public/resources/themes/' + theme + '.min.css';
 
+    var indexCSS = document.createElement('link');
+
+    // set the attributes for link element
+    indexCSS.rel = 'stylesheet';
+    indexCSS.type = 'text/css';
+    indexCSS.href = '/public/resources/index.css';
+    
     // Get HTML head element to append
     // link element to it
-    document.getElementsByTagName('HEAD')[0].appendChild(link);
+    var head = document.getElementsByTagName('HEAD')[0]
+    head.appendChild(themeCSS);
+    head.appendChild(indexCSS);
 }
